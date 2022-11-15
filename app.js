66eda0de45887625d8c457ef1c444d086e607d77
Youssef Hossny <87088905+ussifhossny@users.noreply.github.com>
2022-11-15 23:00:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ let yesBtn = document.getElementById("yesBtn");
 let home = document.getElementById("home");
 let page2 = document.getElementById("page2")
 let page3 = document.getElementById("page3")
+let page4 = document.getElementById("page4")
 let howBtn = document.getElementById("howBtn");
+let noThanks = document.getElementById("noThanks");
+
 
 
 yesBtn.addEventListener("click", () => {
@@ -13,3 +16,9 @@ howBtn.addEventListener("click", () => {
     page2.classList.add("hide")
     page3.classList.remove("hide")
 });
+
+noThanks.addEventListener("click", () => {
+    page2.classList.add("hide");
+    page4.classList.remove("hide")
+    document.body.style.background = "black"; 
+})
